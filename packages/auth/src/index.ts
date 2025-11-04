dae2579a632a9eed6b4e917fdd355eaefec740c9
antonio-jsx <antoniodev.1993@gmail.com>
2025-11-04 14:28:07 -0300

    feat(auth): add role admin

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,11 +2,12 @@ import { db } from '@bakan/database';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { nextCookies } from 'better-auth/next-js';
+import { admin } from 'better-auth/plugins';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: 'pg', usePlural: true }),
   emailAndPassword: {
     enabled: true,
   },
-  plugins: [nextCookies()],
+  plugins: [nextCookies(), admin()],
 });
