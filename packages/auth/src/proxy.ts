e2ae8d3098e58a937d87aabe68546dd4d49a05a5
antonio-jsx <antoniodev.1993@gmail.com>
2025-11-11 20:49:16 -0300

    fix: handler is not necessary

--- packages/auth/src/proxy.ts
+++ packages/auth/src/proxy.ts
@@ -6,15 +6,20 @@ type MiddlewareHandler = (
 ) => NextResponse | Response | Promise<NextResponse | Response>;
 
 export function withProtectedRoute(
-  handler: MiddlewareHandler
+  handler?: MiddlewareHandler
 ): MiddlewareHandler {
   return async (request: NextRequest) => {
     const { pathname } = request.nextUrl;
+
     if (pathname.startsWith('/dashboard')) {
       const cookie = getSessionCookie(request);
       if (!cookie) {
         return NextResponse.redirect(new URL('/signin', request.url));
       }
+    }
+
+    if (!handler) {
+      return NextResponse.next();
     }
 
     return handler(request);
