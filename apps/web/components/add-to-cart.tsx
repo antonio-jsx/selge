cec04dbee0277c1b2e804585bd560f87afebf46d
antonio-jsx <antoniodev.1993@gmail.com>
2025-11-14 18:18:15 -0300

    refactor: add toast message

--- apps/web/components/add-to-cart.tsx
+++ apps/web/components/add-to-cart.tsx
@@ -3,13 +3,21 @@
 import { useShopping } from '@/store/shopping';
 import type { SelectProduct } from '@bakan/database/schemas/products';
 import { Button } from '@bakan/ui/components/button';
+import { toast } from '@bakan/ui/components/sonner';
 import { ShoppingCartIcon } from 'lucide-react';
 
 export function AddToCart({ product }: { product: SelectProduct }) {
   const { addToCart } = useShopping();
 
+  const handleCard = () => {
+    addToCart({ ...product, quantity: 1 });
+    toast.success('Product added to cart', {
+      description: 'You can view your cart to proceed to checkout.',
+    });
+  };
+
   return (
-    <Button onClick={() => addToCart({ ...product, quantity: 1 })}>
+    <Button onClick={handleCard}>
       <ShoppingCartIcon /> Add to cart
     </Button>
   );
