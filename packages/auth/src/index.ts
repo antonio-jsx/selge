f74919406a0fc81e7c6fdc105d981059cbbc2f8c
antonio-jsx <antoniodev.1993@gmail.com>
2025-12-10 12:47:09 -0300

    chore: update dependencies

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,6 +1,6 @@
 import { db } from '@selge/database';
-import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { betterAuth } from 'better-auth/minimal';
 import { nextCookies } from 'better-auth/next-js';
 import { admin } from 'better-auth/plugins';
 
